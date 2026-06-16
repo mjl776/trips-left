1e398adea293d11dd9f4aa55d571cbc9486c95e7
Michael Lee <mlee@MacBook-Pro.local>
2026-06-16 15:41:05 -0400

    switched backend to nestjs

--- backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Initialize the FastAPI app instance
-app = FastAPI()
-
-# Define a simple GET path operation
-@app.get("/")
-def read_root():
-    return {"message": "Hello Welcome to the backend of the trips left application"}
