bf3ed8e5e7a36936b3d8e1b46014c66be8360d9f
Michael Lee <mlee@MacBook-Pro-3.local>
2026-07-10 17:16:14 -0400

    backfill defensive stats in python script

---
+++ backend/prisma/migrations/20260710200222_add_def_stats_to_player_stats/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "player_stats" ADD COLUMN     "def_fum_rec" DECIMAL(5,2),
+ADD COLUMN     "def_int" DECIMAL(5,2),
+ADD COLUMN     "def_pa_allow" DECIMAL(5,2),
+ADD COLUMN     "def_sack" DECIMAL(5,2),
+ADD COLUMN     "def_safety" DECIMAL(5,2),
+ADD COLUMN     "def_td" DECIMAL(5,2);
