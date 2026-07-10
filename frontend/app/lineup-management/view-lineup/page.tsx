e7baa7c107bc2755b5b870302c41ba46f8790471
mjl776 <53539497+mjl776@users.noreply.github.com>
2026-07-09 23:37:15 -0400

    fix search params issue (#26)
    Co-authored-by: Michael Lee <mlee@MacBook-Pro-3.local>

--- frontend/app/lineup-management/view-lineup/page.tsx
+++ frontend/app/lineup-management/view-lineup/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from 'react'
 import ViewLineupPanel from '@/components/ViewLineupPanel'
 import styles from './page.module.css'
 
 export default function ViewLineupPage() {
     return (
         <div className={styles.container}>
-            <ViewLineupPanel />
+            <Suspense>
+                <ViewLineupPanel />
+            </Suspense>
         </div>
     )
 }
